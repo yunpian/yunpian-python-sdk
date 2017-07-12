b617a334119b8e4278c2faf22b1e2f40937d6e8e
dzh <dzh_11@qq.com>
2017-07-12 14:19:58 +0800

    doc 1.0.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README.rst'), encodin
 
 KWARGS = dict(
     name='yunpian-python-sdk',
-    version='1.0.0',
+    version='1.0.1',
     description='Yunpian Python SDK',
     long_description=LONG_DESC,
     author='dzh',
